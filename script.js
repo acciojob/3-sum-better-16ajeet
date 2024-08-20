0fee62c274a5b657ef3d85cd146ab7b6218d8325
Ajeet Kumar <98801990+16ajeet@users.noreply.github.com>
2024-08-20 08:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+// Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers.
+// Assume that there will only be one solution. You only need to complete the threeSum function inside script.js
+// Example: given array S = {-1 2 1 -4},
+// and target = 1.
+// The sum that is closest to the target is 2. (-1 + 2 + 1 = 2)
+
 function threeSum(arr, target) {
-// write your code here
-  
+    // write your code here
+    let operatingArray = [...arr]
+    operatingArray.push(arr[0], arr[1])
+    // console.log(operatingArray)
+    let res = []
+
+    for (let index = 0; index < operatingArray.length-2; index++) {
+        const sum = operatingArray[index] + operatingArray[index+1] + operatingArray[index+2];
+        // console.log(sum)
+        res.push(sum);
+        
+    }
+
+    res.push(target)
+    res.sort((a,b) => a-b);  // 2,-1, -4, -3 
+    // console.log(res);
+    let n = res.indexOf(target)
+    // console.log(n)
+    let ans = 0;
+    if ((res[n] - res[n-1]) < (res[n+1] - res[n]) ) {
+        ans = res[n]
+    } else {
+        ans = res[n+1]
+    }
+    console.log(ans)
+    
+    
+      
 }
+
+
 
 module.exports = threeSum;
